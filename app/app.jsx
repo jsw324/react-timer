eedf4e8795183526052b6cf062b8aa534346d4d8
[jsw324] <jwalkow@tallience.com>
2017-03-14 13:19:55 -0400

    Support foundation settings

--- app/app.jsx
+++ app/app.jsx
@@ -6,7 +6,6 @@ const Timer = require('Timer');
 const Countdown = require('Countdown');
 
 // Load foundation
-require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
 
 //App css
